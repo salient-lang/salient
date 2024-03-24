0e27372967716ccba206f45c5e9b7e961a2fcea2
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2024-03-24 15:18:50 +1100

    better operator precedence test

--- tests/e2e/compiler/numeric.test.ts
+++ tests/e2e/compiler/numeric.test.ts
@@ -1,18 +1,17 @@
 /// <reference lib="deno.ns" />
-import { fail, assertEquals, assertNotEquals, assert } from "https://deno.land/std@0.201.0/assert/mod.ts";
+import { fail, assertNotEquals, assert } from "https://deno.land/std@0.201.0/assert/mod.ts";
 
 import * as CompilerFunc from "~/compiler/function.ts";
 import Package from "~/compiler/package.ts";
 import Project from "~/compiler/project.ts";
 import { FuncRef } from "~/wasm/funcRef.ts";
 
-const decoder = new TextDecoder();
-
-const goalStdout = "";
-
 const source = `
-fn main(): f32 {
-	return -3.5 % 2.0;
+fn main(): bool {
+	// (-2.5 % 2.0) * -3.0 == 10.0 - ((1.0 / 2.0) % 10.0) - 8.0;
+	// 1.5 == 1.5
+	// true == 1
+	return -2.5 % 2.0 * -3.0 == 10.0 - 1.0 / 2.0 % 10.0 - 8.0;
 }`;
 
 Deno.test(`Numeric logic test`, async () => {
@@ -27,48 +26,17 @@ Deno.test(`Numeric logic test`, async () => {
 	assertNotEquals(mainFunc.ref, null, "Main function hasn't compiled");
 	project.module.exportFunction("_start", mainFunc.ref as FuncRef);
 
-	let stdout = "";
-	let memory: WebAssembly.Memory;
-
-	const imports = {
-		wasi_snapshot_preview1: {
-			fd_write: (fd: number, iovs: number, iovs_len: number, n_written: number) => {
-				const memoryArray = new Int32Array(memory.buffer);
-				const byteArray   = new Uint8Array(memory.buffer);
-				for (let iovIdx = 0; iovIdx < iovs_len; iovIdx++) {
-					const bufPtr = memoryArray.at(iovs/4 + iovIdx*2) || 0;
-					const bufLen = memoryArray.at(iovs/4 + iovIdx*2 + 1) || 0;
-					const data = decoder.decode(byteArray.slice(bufPtr, bufPtr + bufLen));
-					stdout += data;
-				}
-				return 0; // Return 0 to indicate success
-			}
-		}
-	};
-
-	// Load the wasm module
 	const wasmModule = new WebAssembly.Module(project.module.toBinary());
+	const instance = await WebAssembly.instantiate(wasmModule, {});
 
-	try {
-		// Instantiate the wasm module
-		const instance = await WebAssembly.instantiate(wasmModule, imports);
+	const exports = instance.exports;
 
-		const exports = instance.exports;
-		memory = exports.memory as WebAssembly.Memory;
+	// Call the _start function
+	let main: () => number = typeof exports._start === "function"
+		? exports._start as any
+		: fail(`Expected _start to be a function`);
 
-		// Call the _start function
-		if (typeof exports._start === "function") {
-			(exports._start as Function)() as any;
-		} else {
-			fail(`Expected _start to be a function`);
-		}
-
-		// Check stdout
-		assertEquals(stdout, goalStdout);
-
-	} catch (err) {
-		// If there's an error, the test will fail
-		fail(`Failed to run wasm module: ${err}`);
-	}
+	const code = main() as number;
+	if (code === 0) fail(`equivalence checks failed`);
 
 });
